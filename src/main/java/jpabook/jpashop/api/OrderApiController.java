26124bef12880da2bcea2d636341d0d8dd53fdfa
Soomin Kim <soomin0403@naver.com>
2021-12-06 23:57:49 +0900

    2021.12.06 [컬렉션 조회 최적-2]

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -77,8 +77,13 @@ public class OrderApiController {
     }
 
     @GetMapping("/api/v3/orders")
-    public List<OrderDto> ordersV3() {
-        return null;
+    public List<OrderDto> ordefinrsV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
     }
     @GetMapping("/api/v4/orders")
     public List<OrderDto> ordersV4() {
--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -100,8 +100,16 @@ public class OrderRepository {
         return em.createQuery(
                 "select o from Order o" +
                         " join fetch o.member m" +
-                        " join fetch o.delivery d", Order.class)
+                        " join fetch o.delivery d",Order.class)
                 .getResultList();
+    }
 
+    public List<Order> findAllWithItem() {
+        return em.createQuery("select distinct o from Order o" +
+                "join fetch o.memnber ," +
+                "join fetch o.delivery d" +
+                "join fetch o.orderItem oi" +
+                "join fetch oi.item i", Order.class)
+                .getResultList();
     }
 }
